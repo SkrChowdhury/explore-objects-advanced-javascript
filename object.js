544499983d7bd69ff616f54418a287afd890c040
SkrChowdhury <sakkhor.chowdhury@gmail.com>
2022-09-02 12:15:31 +0600

    5 ways to create objects

--- object.js
+++ object.js
@@ -9,7 +9,6 @@ player.speciality = 'Batsman';
 player.bat = function(){
     console.log('Swing Your Bat');
 }
-
 console.log(player);
 player.bat();
 
@@ -25,4 +24,25 @@ console.log(person)
 // 3. object create method 
 const item = Object.create(null);
 const atel = Object.create(student);
-console.log(atel)
+console.log(atel);
+console.log(atel.salary);
+
+// 4. class
+class Person {
+    name = 'abul';
+    address = "sodor ghat";
+    constructor(age){
+       this.age = age; 
+    }
+}
+
+const person1 = new Person(34);
+console.log(person1);
+
+// 5. function 
+function Car (model, price){
+    this.model  = model;
+    this.price = price;
+}
+
+const tesla = new Car ('Elon Musk', 32);
